777905379f15e0416a9089b8820bca7e44d85a29
michellelevesquework <michelle@mlevesque.ca>
2026-08-17 17:52:37 -0500

    Update portfolio links with new resource URLs

--- links.js
+++ links.js
@@ -1,7 +1,14 @@
 window.PORTFOLIO_LINKS = {
-  smartGoalsManagerGuide: "#",
-  smartGoalsWorksheet: "#",
-  smartGoalsFacilitatorDeck: "#",
-  teamAlignmentFacilitatorGuide: "#",
-  teamAlignmentOKRGuide: "#"
+  // Learning Design Resources
+  learningDesignFramework: "https://drive.google.com/file/d/1AyT8WBMYkRJVDCBU3D9CIKBi27Bv45h2/view?usp=sharing",
+  learningNeedsModalities: "https://drive.google.com/file/d/1LBV-_-NnBVs1RrsdUTwn4oTMyYKg8D3a/view?usp=sharing",
+
+  // Team Alignment Sprint
+  teamAlignmentFacilitatorGuide: "https://drive.google.com/file/d/1wblvT0rNbziR_7IipD1gOWmJhG4DNCRd/view?usp=sharing",
+  teamAlignmentOKRGuide: "https://drive.google.com/file/d/14pzdZkRIUd2WrPWRLdsMQcyYRz4gKqnQ/view?usp=sharing",
+
+  // Writing SMART Goals
+  smartGoalsFacilitatorDeck: "https://docs.google.com/presentation/d/1lUL75FWcihqX8ZlLm3v44ojQSgKONK7-K8xwLULejlQ/view",
+  smartGoalsWorksheet: "https://docs.google.com/document/d/14gy95HEqHYiOm-VPJQyt7CRt7_ZlSi-VKkzITQ1E-IU/view",
+  smartGoalsManagerGuide: "https://docs.google.com/document/d/1a1Qb3h-UgdaO-kOFPRu8DgUIKWr0raeJr97tHFUXCBI/view"
 };
